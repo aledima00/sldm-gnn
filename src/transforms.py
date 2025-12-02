133829bddd4d67db7f0794362313eb33e5dd6262
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-12-02 02:16:46 +0100

    redefined transforms in new format

--- src/transforms.py
+++ src/transforms.py
@@ -1,104 +1,92 @@
 import torch as _tch
 from torch_geometric.data import Data as _GData
 
+from .utils import MetaData as _MD, FmaskType as _FMType
+
 # define useful transformations for dataset
 
-# x features: [*[Xt, Yt, Speedt, HeadingT, PresenceFlagt, tsin, tcos]*20, width, length]
-xmask = [True,False,False,False,False,False,False]*20 + [False, False]
-ymask = [False,True,False,False,False,False,False]*20 + [False, False]
-speed_mask = [False,False,True,False,False,False,False]*20 + [False, False]
-heading_mask = [False,False,False,True,False,False,False]*20 + [False, False]
-wmask = [False]*7*20 + [True, False]
-lmask = [False]*7*20 + [False, True]
-tmask = [True]*7*20 + [False, False]
-
-class PosNoise:
-    def __init__(self, std:float, device:str='cpu'):
+class AddNoise:
+    def __init__(self,target:_FMType, std:float, metadata:_MD):
         self.std = std
-        self.device = device
+        self.mask = metadata.getDataMask(target)
+        self.flattenedTime = metadata.flattened_time
     
     def __call__(self, data:_GData)->_GData:
-        data.x[:,xmask] += _tch.randn_like(data.x[:,xmask],device=self.device) * self.std
-        data.x[:,ymask] += _tch.randn_like(data.x[:,ymask],device=self.device) * self.std
-        return data
-    
-class SpeedNoise:
-    def __init__(self, std:float, device:str='cpu'):
-        self.std = std
-        self.device = device
-    def __call__(self, data:_GData)->_GData:
-        data.x[:,speed_mask] += _tch.randn_like(data.x[:,speed_mask], device=self.device) * self.std
-        return data
-
-class HeadingNoise:
-    def __init__(self, std:float, device:str='cpu'):
-        self.std = std
-        self.device = device
-    def __call__(self, data:_GData)->_GData:
-        data.x[:,heading_mask] += _tch.randn_like(data.x[:,heading_mask], device=self.device) * self.std
+        pmsk = self.mask.unsqueeze(0).expand(data.x.shape[0],-1,-1) if not self.flattenedTime else self.mask.unsqueeze(0).expand(data.x.shape[0],-1)
+        data.x[pmsk] += _tch.randn_like(data.x[pmsk],device=data.x.device) * self.std
         return data
     
 class RemoveDimsFeatures:
+    def __init__(self,metadata:_MD):
+        self.hasDims = metadata.has_dims
+        
     def __call__(self, data:_GData)->_GData:
-        data.x = data.x[:, tmask]
-        return data
-    
-class RescalePosToVCenter:
-    def __init__(self):
-        pass
-    def __call__(self, data:_GData)->_GData:
-        # rescale X,Y positions so that (0,0) is the center of the vehicle
-
-        # extract dims
-        l = data.x[:,lmask]
-        h = data.x[:,heading_mask]
-        x = data.x[:,xmask]
-        y = data.x[:,ymask]
-
-        # rescale
-        x = x - (l/2) * _tch.cos(h)
-        y = y - (l/2) * _tch.sin(h)
-
-        # save
-        data.x[:,xmask] = x
-        data.x[:,ymask] = y
+        if self.hasDims:
+            # remove width and length from static features (first 2 features)
+            del data.xdims
         return data
     
 class RandomRotate:
-    def __init__(self,num_frames:int=20,device:str='cpu'):
-        self.num_frames = num_frames
-        self.device = device
+    def __init__(self, metadata:_MD):
+        self.flattenedTime = metadata.flattened_time
+        self.num_frames = metadata.frames_num
+        self.posMask = metadata.getDataMask('pos')
+        self.hMask = metadata.getDataMask('heading')
+        self.hSinMask = metadata.getDataMask('hsin')
+        self.hCosMask = metadata.getDataMask('hcos')
+        self.headingEncoded = metadata.heading_encoded
     def __call__(self, data:_GData)->_GData:
         # random rotation angle
-        theta = _tch.rand(1).item() * 2 * _tch.pi
-        cos_theta = _tch.cos(_tch.tensor(theta, device=self.device))
-        sin_theta = _tch.sin(_tch.tensor(theta, device=self.device))
-
-        # extract positions (still temporal sequence)
-        xs = data.x[:,xmask]
-        ys = data.x[:,ymask]
-
-        # rotation matrix
+        dev = data.x.device
+        theta = _tch.rand(1,device=dev).item() * 2 * _tch.pi
+        cos_theta = _tch.cos(_tch.tensor(theta, device=dev))
+        sin_theta = _tch.sin(_tch.tensor(theta, device=dev))
+        # define rotation matrix
         RotMat = _tch.tensor([
             [cos_theta, -sin_theta],
             [sin_theta, cos_theta]
-        ], device=self.device)
+        ], device=dev)
 
-        # rotate positions
-        for i in range(self.num_frames):
-            coords = _tch.stack([xs[:,i], ys[:,i]], dim=1)  # shape [num_nodes, 2]
-            rotated_coords = coords @ RotMat.T  # shape [num_nodes, 2]
-            xs[:,i] = rotated_coords[:,0]
-            ys[:,i] = rotated_coords[:,1]
-        data.x[:,xmask] = xs
-        data.x[:,ymask] = ys
+        if self.flattenedTime:
+            # extract positions (still temporal sequence)
+            positions = data.x[:,self.posMask]
+
+            # rotate positions
+            for i in range(self.num_frames):
+                coords = positions[:,i*2:(i*2)+2]  # shape [num_nodes, 2]
+                rotcoords = coords @ RotMat.T  # shape [num_nodes, 2]
+                positions[:,i*2:(i*2)+2] = rotcoords
+            data.x[:,self.posMask] = positions
+        else:
+            # extract positions
+            positions = data.x[:,:,self.posMask]
+            # rotate positions
+            positions = positions @ RotMat.T  # shape [num_nodes, num_features, 2]
+            data.x[:,:,self.posMask] = positions
 
         # adjust headings
-        headings = data.x[:,heading_mask]
-        headings = (headings + theta) % (2 * _tch.pi)
-        data.x[:,heading_mask] = headings
+        if self.headingEncoded:
+            if self.flattenedTime:
+                hsin = data.x[:,self.hSinMask]
+                hcos = data.x[:,self.hCosMask]
+                data.x[:,self.hSinMask] = hsin*cos_theta + hcos*sin_theta
+                data.x[:,self.hCosMask] = hcos*cos_theta - hsin*sin_theta
+            else:
+                hsin = data.x[:,:,self.hSinMask]
+                hcos = data.x[:,:,self.hCosMask]
+                data.x[:,:,self.hSinMask] = hsin*cos_theta + hcos*sin_theta
+                data.x[:,:,self.hCosMask] = hcos*cos_theta - hsin*sin_theta
+        else:
+            if self.flattenedTime:
+                headings = data.x[:,self.hMask]
+                data.x[:,self.hMask] = (headings + theta) % (2 * _tch.pi)
+            else:
+                headings = data.x[:,:,self.hMask]
+                data.x[:,:,self.hMask] = (headings + theta) % (2 * _tch.pi)
+        
 
         return data
         
         
 
+__all__ = ['AddNoise','RemoveDimsFeatures','RandomRotate']
