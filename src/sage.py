f2b84486c284cbefba9859f00e62db8f873bad3e
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-12-02 19:30:40 +0100

    added fc part in sage

--- src/sage.py
+++ src/sage.py
@@ -4,17 +4,25 @@ from torch_geometric.nn import SAGEConv, global_mean_pool, global_max_pool
 import torch.nn.functional as F
 
 class GraphSAGEGraphLevel(nn.Module):
-    def __init__(self, in_dim, hidden_dims=[128, 128], out_dim=10, num_st_types=256, emb_dim=12, dropout=0.1):
+    def __init__(self, in_dim, hidden_dims=[128, 128], fcdims=[50,50], out_dim=10, num_st_types=256, emb_dim=12, dropout=0.1):
         super().__init__()
         self.st_emb = nn.Embedding(num_st_types, emb_dim)
-        dims = [in_dim + emb_dim] + hidden_dims
+        cdims = [in_dim + emb_dim] + hidden_dims
         self.convs = nn.ModuleList([
-            SAGEConv(dims[i], dims[i+1]) for i in range(len(hidden_dims))
+            SAGEConv(cdims[i], cdims[i+1]) for i in range(len(cdims)-1)
         ])
         self.norms = nn.ModuleList([
-            nn.LayerNorm(dim) for dim in hidden_dims
+            nn.LayerNorm(cdims[i+1]) for i in range(len(cdims)-1)
         ])
-        self.lin = nn.Linear(dims[-1]*2, out_dim)
+
+        ldims = [hidden_dims[-1]*2] + fcdims + [out_dim]
+        self.fcs = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(ldims[i], ldims[i+1]),
+                nn.ReLU(),
+                nn.Dropout(p=dropout)
+            ) for i in range(len(ldims)-1)
+        ])
         self.dropout = dropout
 
     def forward(self, data):
@@ -26,8 +34,11 @@ class GraphSAGEGraphLevel(nn.Module):
             x = norm(x)
             x = F.relu(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
+        # graph level readout
         xmean = global_mean_pool(x, batch)
         xmax = global_max_pool(x, batch)
         x = torch.cat([xmean, xmax], dim=1)
-        logits = self.lin(x)
-        return logits
+        # now fc layers
+        for fc in self.fcs:
+            x = fc(x)
+        return x
