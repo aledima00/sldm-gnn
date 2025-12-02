8f2240a30a7e392a84494f68a13d9adfa2ebf24c
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-12-02 02:04:57 +0100

    refactored main, moved functions to utils.py, added Metadata loading

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import torch
-from torch.utils.data import random_split
 from torch_geometric.loader import DataLoader as GDL
 import torch_geometric.transforms as T
 from pathlib import Path
-from src.dataset import MapGraph
-from src.sage import GraphSAGEGraphLevel
-from src.gat import GATGraphLevel
 import colorama
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 import numpy as np
-import src.transforms as TFs
-from src.labels import LabelsEnum
-from src.tprint import TabPrint
 from typing import Literal as Lit
-from tqdm.auto import tqdm
 from matplotlib import pyplot as plt
 import click
 
+from src.dataset import MapGraph
+from src.sage import GraphSAGEGraphLevel
+from src.gat import GATGraphLevel
+import src.transforms as TFs
+from src.labels import LabelsEnum
+from src.utils import train_model, Progress_logging_options, split_tr_ev_3to1, MetaData
+
 colorama.init(autoreset=True)
+
+
 
 # data description
 EMB_DIM = 12
 NUM_POSSIBLE_STATION_TYPES = 256
-NUM_TEMPORAL_FEATURES = 7
+NUM_TEMPORAL_FEATURES = 8
 NUM_STATIC_FEATURES = 2
 FRAMES_PER_PACK = 20
 
@@ -32,6 +33,7 @@ DF_BATCH_SIZE = 32
 DF_LR = 0.005
 DF_WEIGHT_DECAY = 5e-4
 DF_ACTIVE_LABELS = [0,1,2,3,4,5,6,7,8]
+DF_DROPOUT = 0.2
 
 # gnn parameters
 SAGE_HIDDEN_DIMS = [128, 64]
@@ -42,117 +44,10 @@ GAT_ATTENTION_HEADS = 8
 # device
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def getLbName(label_idx:int,active_labels)->str:
-    try:
-        return LabelsEnum(active_labels[label_idx]).name
-    except ValueError:
-        return "UNKNOWN_LABEL"
-
-def split_tr_ev_3to1(dataset:MapGraph)->tuple[MapGraph,MapGraph]:
-    total_len = len(dataset)
-    train_len = (total_len * 3) // 4
-    val_len = total_len - train_len
-    train_ds, val_ds = random_split(dataset, [train_len, val_len])
-    return train_ds, val_ds
-
-Progress_logging_options = Lit['clilog', 'tqdm', 'none']
 Model_opts_type = Lit['sage','gat']
 
-def train_model(model:torch.nn.Module, train_loader:GDL, eval_loader:GDL, epochs:int=10, lr:float=1e-3, weight_decay:float=1e-5, device:str='cpu', verbose:bool=False, *, progress_logging:Progress_logging_options='clilog', active_labels):
-    model = model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    criterion = torch.nn.BCEWithLogitsLoss()
-    tprint = TabPrint(tab="   ", enabled=(progress_logging=='clilog'))
-
-    act_labels_num = len(active_labels)
-
-    pl_tracc = np.zeros((act_labels_num,epochs), dtype=np.float32)
-    tot_tracc = np.zeros((1,epochs), dtype=np.float32)
-    pl_vacc = np.zeros((act_labels_num,epochs), dtype=np.float32)
-    tot_vacc = np.zeros((1,epochs), dtype=np.float32)
-
-    for epoch in tqdm(range(epochs), desc="Training Epochs", disable=(progress_logging!='tqdm')):
-        tprint(f"\n{Back.CYAN}{Fore.YELLOW} ---------- Epoch {epoch+1}/{epochs} ---------- {Style.RESET_ALL}")
-        with tprint.tab:
-            tprint(f"{Fore.YELLOW}{Style.BRIGHT}--> Training...   {Style.RESET_ALL}")
-            with tprint.tab:
-                model.train()
-                train_total_loss = 0
-                tot_mlb = 0
-                tot_correct = torch.zeros((1,act_labels_num), device=device, dtype=torch.long)
-                for batch in train_loader:
-                    batch = batch.to(device)
-                    optimizer.zero_grad()
-                    logits = model(batch)
-                    scores = torch.sigmoid(logits)
-                    y = batch.y.float().view(batch.num_graphs, act_labels_num)
-                    train_loss = criterion(logits, y)
-                    train_loss.backward()
-                    train_total_loss += train_loss.item() * batch.num_graphs
-                    optimizer.step()
-
-                    # Accuracy con threshold 0.5
-                    preds = (scores >= 0.5).float()
-                    corr = (preds == y).long().sum(dim=0)
-                    tot_correct += corr
-                    tot_mlb += batch.num_graphs
-                    acc = corr.sum().item() / (batch.num_graphs * act_labels_num)
-                    if verbose:
-                        tprint(f"{Style.DIM}Training Batch Loss: {train_loss.item():.4f}, Training Batch Accuracy: {acc:.4f}{Style.RESET_ALL}")
-            avg_train_loss = train_total_loss / len(train_loader)
-            if verbose:
-                tprint(f"Training Loss: {avg_train_loss:.4f}")
-            tot_train_accuracy = tot_correct.sum().item() / (tot_mlb * act_labels_num)
-            per_label_train_acc = (tot_correct.sum(dim=0).cpu().float().numpy() / tot_mlb).tolist()
-            tprint(f"{Fore.GREEN}{Style.BRIGHT}Training Accuracy: {tot_train_accuracy:.4f}{Style.RESET_ALL}")
-            if verbose:
-                tprint(f"Per-Label Training Accuracy:")
-                with tprint.tab:
-                    for i, acc in enumerate(per_label_train_acc):
-                        tprint(f'label "{getLbName(i, active_labels)}" -> {acc:.4f}')
-
-            # Evaluation
-            tprint(f"{Fore.YELLOW}{Style.BRIGHT}--> Validating ...   {Style.RESET_ALL}")
-            with tprint.tab:
-                model.eval()
-                val_total_loss = 0
-                tot_mlb = 0
-                tot_correct = torch.zeros((1,act_labels_num), device=device, dtype=torch.long)
-
-                with torch.no_grad():
-                    for batch in eval_loader:
-                        batch = batch.to(device)
-                        logits = model(batch)
-                        scores = torch.sigmoid(logits)
-                        y = batch.y.float().view(batch.num_graphs, act_labels_num)
-                        val_loss = criterion(logits, y)
-                        val_total_loss += val_loss.item() * batch.num_graphs
-
-                        # Accuracy con threshold 0.5
-                        preds = (scores >= 0.5).float()
-                        tot_correct += (preds == y).long().sum(dim=0)
-                        tot_mlb += batch.num_graphs
-            avg_val_loss = val_total_loss / len(eval_loader)
-            if verbose:
-                tprint(f"Validation Loss: {avg_val_loss:.4f}")
-            tot_val_accuracy = tot_correct.sum().item() / (tot_mlb * act_labels_num)
-            per_label_val_acc = (tot_correct.sum(dim=0).cpu().float().numpy() / tot_mlb).tolist()
-            tprint(f"{Fore.GREEN}{Style.BRIGHT}Validation Accuracy: {tot_val_accuracy:.4f}{Style.RESET_ALL}")
-            tprint(f"Per-Label Eval Accuracy:")
-            with tprint.tab:
-                for i, acc in enumerate(per_label_val_acc):
-                    tprint(f'label "{getLbName(i, active_labels)}" -> {acc:.4f}')
-        pl_tracc[:,epoch] = np.array(per_label_train_acc)
-        pl_vacc[:,epoch] = np.array(per_label_val_acc)
-        tot_tracc[:,epoch] = tot_train_accuracy
-        tot_vacc[:,epoch] = tot_val_accuracy
-    return (pl_tracc, tot_tracc), (pl_vacc, tot_vacc)
-
-def rungnn(dirpath, verbose:bool=False, model:Model_opts_type='sage', progress_logging:Progress_logging_options='clilog',*,active_labels=DF_ACTIVE_LABELS, wd:float=DF_WEIGHT_DECAY,lr:float=DF_LR,bs:int=DF_BATCH_SIZE, epochs:int=DF_EPOCHS, transform=None,dropout:float=0.2,hidden_dims:list[int]=[32, 32],emb_dim:int=EMB_DIM):
+def rungnn(ds:MapGraph, verbose:bool=False, model:Model_opts_type='sage', progress_logging:Progress_logging_options='clilog',*,active_labels, wd:float,lr:float,bs:int, epochs:int,dropout:float,hidden_dims:list[int],emb_dim:int):
     in_dim = FRAMES_PER_PACK * NUM_TEMPORAL_FEATURES  + NUM_STATIC_FEATURES
-    # load data
-    dpath = Path(dirpath).resolve() / '.graphs'
-    ds = MapGraph(dpath,frames_num=FRAMES_PER_PACK, active_labels=active_labels, device=DEVICE, transform=transform, normalizeZScore=True)
     print(f" - Using device: {DEVICE}")
     print(f" - Dataset length: {len(ds)}")
 
@@ -255,14 +150,18 @@ def autorun():
 @click.option('-l', '--label-num', 'lbnum', type=int, required=True, prompt='Label number to train the model on')
 def runModel(inputdir,outdir,lbnum:int):
     path = inputdir.resolve()
+    gpath = path / '.graphs'
+    metapath = gpath / 'metadata.json'
+    metadata = MetaData.loadJson(metapath)
     outpath = outdir.resolve()
     outpath.mkdir(parents=True, exist_ok=True)
+
     plt_yticks = np.arange(-0.1, 1.2, 0.1)
 
     lr = 4e-3
     bs = 32
     wd = 1e-5
-    epochs = 100
+    epochs = 10
     dropout = 0.3
     hidden_dims = [128, 64]
     emb_dim = 10
@@ -272,17 +171,18 @@ def runModel(inputdir,outdir,lbnum:int):
     params_str = f"LR{lr:.2e}_BS{bs:02d}_WD{wd:.2e}_EP{epochs:02d}_DRP{dropout:.2f}_HID{'x'.join([str(h) for h in hidden_dims])}_EMB{emb_dim:02d}_PNS{pos_noise:.2e}"\
         .replace('.','p')\
         .replace('-','m')
-
-
-
+    
     transform = T.Compose([
-        TFs.RandomRotate(num_frames=20, device=DEVICE),
-        TFs.RescalePosToVCenter(),
-        TFs.PosNoise(std=pos_noise,device=DEVICE)        
+        TFs.RandomRotate(metadata=metadata),
+        TFs.AddNoise(target='pos', std=pos_noise, metadata=metadata),
     ])
 
+    # dataset and transforms
+    ds = MapGraph(gpath, device=DEVICE, transform=transform, normalizeZScore=True, metadata=metadata)
+    
+
     (_, tot_tracc),(_, tot_vacc) = rungnn(
-        path,
+        ds,
         verbose=False,
         model='sage',
         progress_logging='clilog',
@@ -291,7 +191,6 @@ def runModel(inputdir,outdir,lbnum:int):
         lr=lr,
         bs=bs,
         epochs=epochs,
-        transform=transform,
         dropout=dropout,
         hidden_dims=hidden_dims,
         emb_dim=emb_dim
---
+++ src/utils.py
@@ -0,0 +1,178 @@
+import torch as _tch
+from torch.utils.data import random_split as _rsplit
+from torch_geometric.loader import DataLoader as _GDL
+from torch_geometric.data import Dataset as _GDataset
+from typing import Literal as _Lit
+from colorama import Fore as _Fore, Back as _Back, Style as _Style
+from tqdm.auto import tqdm as _tqdm
+import numpy as _np
+from dataclasses import dataclass as _dc
+from pathlib import Path as _Path
+import json as _json
+
+from .tprint import TabPrint as _TabPrint
+from .labels import LabelsEnum as _LE
+
+Progress_logging_options = _Lit['clilog', 'tqdm', 'none']
+
+FmaskType = _Lit['x','y','pos','speed','heading','hsin','hcos']
+
+@_dc
+class MetaData:
+    frames_num:int
+    m_radius:float
+    sin_cos_time_enc:bool
+    vpos_rescaled_center:bool
+    has_dims:bool
+    heading_encoded:bool
+    flattened_time:bool
+    active_labels:list[int]
+
+    @staticmethod
+    def loadJson(path:_Path)->'MetaData':
+        mddict:dict = None
+        with open(path.resolve(), 'r', encoding='utf-8') as metafile:
+            mddict = _json.load(metafile)
+        return MetaData(**mddict)
+    
+    def getDataMask(self,selector:FmaskType)->_tch.Tensor:
+        nfeats = 4 + (2 if self.heading_encoded else 1) + (2 if self.sin_cos_time_enc else 0)
+        msk = _tch.full((nfeats,),False,dtype=_tch.bool)
+        match selector:
+            case 'x':
+                msk[0] = True
+            case 'y':
+                msk[1] = True
+            case 'pos':
+                msk[0] = True
+                msk[1] = True
+            case 'speed':
+                msk[2] = True
+            case 'heading':
+                msk[3] = True
+                if self.heading_encoded:
+                    msk[4] = True
+            case 'hsin':
+                if self.heading_encoded:
+                    msk[3] = True
+                else:
+                    raise ValueError("Heading is not encoded with sin/cos, cannot get 'hsin' mask")
+            case 'hcos':
+                if self.heading_encoded:
+                    msk[4] = True
+                else:
+                    raise ValueError("Heading is not encoded with sin/cos, cannot get 'hcos' mask")
+            case _:
+                raise ValueError(f"Unknown selector '{selector}' for getDataMask")
+
+        if self.flattened_time:
+            # repeat mask for all time steps
+            msk = msk.repeat(self.frames_num)
+        else:
+            # add time dimension
+            msk = msk.unsqueeze(0).expand(self.frames_num, -1)
+
+        return msk
+
+def split_tr_ev_3to1(dataset:_GDataset)->tuple[_GDataset,_GDataset]:
+    total_len = len(dataset)
+    train_len = (total_len * 3) // 4
+    val_len = total_len - train_len
+    train_ds, val_ds = _rsplit(dataset, [train_len, val_len])
+    return train_ds, val_ds
+
+def getLbName(label_idx:int,active_labels)->str:
+    try:
+        return _LE(active_labels[label_idx]).name
+    except ValueError:
+        return "UNKNOWN_LABEL"
+
+def train_model(model:_tch.nn.Module, train_loader:_GDL, eval_loader:_GDL, epochs:int=10, lr:float=1e-3, weight_decay:float=1e-5, device:str='cpu', verbose:bool=False, *, progress_logging:Progress_logging_options='clilog', active_labels):
+    model = model.to(device)
+    optimizer = _tch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    criterion = _tch.nn.BCEWithLogitsLoss()
+    tprint = _TabPrint(tab="   ", enabled=(progress_logging=='clilog'))
+
+    act_labels_num = len(active_labels)
+
+    pl_tracc = _np.zeros((act_labels_num,epochs), dtype=_np.float32)
+    tot_tracc = _np.zeros((1,epochs), dtype=_np.float32)
+    pl_vacc = _np.zeros((act_labels_num,epochs), dtype=_np.float32)
+    tot_vacc = _np.zeros((1,epochs), dtype=_np.float32)
+
+    for epoch in _tqdm(range(epochs), desc="Training Epochs", disable=(progress_logging!='tqdm')):
+        tprint(f"\n{_Back.CYAN}{_Fore.YELLOW} ---------- Epoch {epoch+1}/{epochs} ---------- {_Style.RESET_ALL}")
+        with tprint.tab:
+            tprint(f"{_Fore.YELLOW}{_Style.BRIGHT}--> Training...   {_Style.RESET_ALL}")
+            with tprint.tab:
+                model.train()
+                train_total_loss = 0
+                tot_mlb = 0
+                tot_correct = _tch.zeros((1,act_labels_num), device=device, dtype=_tch.long)
+                for batch in _tqdm(train_loader, desc="Training Batches", leave=False):
+                    batch = batch.to(device)
+                    optimizer.zero_grad()
+                    logits = model(batch)
+                    scores = _tch.sigmoid(logits)
+                    y = batch.y.float().view(batch.num_graphs, act_labels_num)
+                    train_loss = criterion(logits, y)
+                    train_loss.backward()
+                    train_total_loss += train_loss.item() * batch.num_graphs
+                    optimizer.step()
+
+                    # Accuracy con threshold 0.5
+                    preds = (scores >= 0.5).float()
+                    corr = (preds == y).long().sum(dim=0)
+                    tot_correct += corr
+                    tot_mlb += batch.num_graphs
+                    acc = corr.sum().item() / (batch.num_graphs * act_labels_num)
+                    if verbose:
+                        tprint(f"{_Style.DIM}Training Batch Loss: {train_loss.item():.4f}, Training Batch Accuracy: {acc:.4f}{_Style.RESET_ALL}")
+            avg_train_loss = train_total_loss / len(train_loader)
+            if verbose:
+                tprint(f"Training Loss: {avg_train_loss:.4f}")
+            tot_train_accuracy = tot_correct.sum().item() / (tot_mlb * act_labels_num)
+            per_label_train_acc = (tot_correct.sum(dim=0).cpu().float().numpy() / tot_mlb).tolist()
+            tprint(f"{_Fore.GREEN}{_Style.BRIGHT}Training Accuracy: {tot_train_accuracy:.4f}{_Style.RESET_ALL}")
+            if verbose:
+                tprint(f"Per-Label Training Accuracy:")
+                with tprint.tab:
+                    for i, acc in enumerate(per_label_train_acc):
+                        tprint(f'label "{getLbName(i, active_labels)}" -> {acc:.4f}')
+
+            # Evaluation
+            tprint(f"{_Fore.YELLOW}{_Style.BRIGHT}--> Validating ...   {_Style.RESET_ALL}")
+            with tprint.tab:
+                model.eval()
+                val_total_loss = 0
+                tot_mlb = 0
+                tot_correct = _tch.zeros((1,act_labels_num), device=device, dtype=_tch.long)
+
+                with _tch.no_grad():
+                    for batch in _tqdm(eval_loader, desc="Validation Batches", leave=False):
+                        batch = batch.to(device)
+                        logits = model(batch)
+                        scores = _tch.sigmoid(logits)
+                        y = batch.y.float().view(batch.num_graphs, act_labels_num)
+                        val_loss = criterion(logits, y)
+                        val_total_loss += val_loss.item() * batch.num_graphs
+
+                        # Accuracy con threshold 0.5
+                        preds = (scores >= 0.5).float()
+                        tot_correct += (preds == y).long().sum(dim=0)
+                        tot_mlb += batch.num_graphs
+            avg_val_loss = val_total_loss / len(eval_loader)
+            if verbose:
+                tprint(f"Validation Loss: {avg_val_loss:.4f}")
+            tot_val_accuracy = tot_correct.sum().item() / (tot_mlb * act_labels_num)
+            per_label_val_acc = (tot_correct.sum(dim=0).cpu().float().numpy() / tot_mlb).tolist()
+            tprint(f"{_Fore.GREEN}{_Style.BRIGHT}Validation Accuracy: {tot_val_accuracy:.4f}{_Style.RESET_ALL}")
+            tprint(f"Per-Label Eval Accuracy:")
+            with tprint.tab:
+                for i, acc in enumerate(per_label_val_acc):
+                    tprint(f'label "{getLbName(i, active_labels)}" -> {acc:.4f}')
+        pl_tracc[:,epoch] = _np.array(per_label_train_acc)
+        pl_vacc[:,epoch] = _np.array(per_label_val_acc)
+        tot_tracc[:,epoch] = tot_train_accuracy
+        tot_vacc[:,epoch] = tot_val_accuracy
+    return (pl_tracc, tot_tracc), (pl_vacc, tot_vacc)
