64239288df36ad2cdf140c1b015e74e12d29cb6c
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-12-03 03:04:19 +0100

    adjusted main, first prototype of GRUGAT

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ colorama.init(autoreset=True)
 # general params
 EMB_DIM = 12
 NUM_POSSIBLE_STATION_TYPES = 256
-NUM_TEMPORAL_FEATURES = 8
 VERBOSE = False
 PROGRESS_LOGGING = 'clilog'  # options: 'clilog', 'tqdm', 'none'
 DF_ACTIVE_LABELS = [0,1,2,3,4,5,6,7,8]
 POS_NOISE_STD = 0.1
 
 # ------------------- SAGE parameters -------------------
+SAGE_NUM_TEMPORAL_FEATURES = 8
 SAGE_HIDDEN_DIMS = [128, 64]
 SAGE_FC_DIMS = [50, 50]
 SAGE_DROPOUT = 0.2
@@ -39,18 +39,19 @@ SAGE_LR = 0.005
 SAGE_WEIGHT_DECAY = 5e-4
 
 # ------------------- GRUGAT parameters -------------------
+GG_NUM_TEMPORAL_FEATURES = 6 # NO SIN-COS TIME ENCODING
 GG_GRU_HIDDEN_SIZE = 128
 GG_GRU_NUM_LAYERS = 1
 GG_GAT_EDGE_FNUM = 1
 GG_GAT_HIDDEN_DIMS = [96,96]
 GG_GAT_NHEADS = 8
-GG_FCDIMS = [50,50]
-GG_DROPOUT = 0.2
+GG_FCDIMS = []
+GG_DROPOUT = 0.05
 GG_NEGSLOPE = 0.2
 # training params
-GG_EPOCHS = 100
+GG_EPOCHS = 10
 GG_BATCH_SIZE = 32
-GG_LR = 0.005
+GG_LR = 1e-3
 GG_WEIGHT_DECAY = 5e-4
 
 # device
@@ -89,7 +90,7 @@ def getPlotFname(model:Lit['sage','gat'], lbnum:int)->str:
     
 @click.command()
 @click.argument('inputdir', type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path), required=True, nargs=1)
-@click.argument('outdir', type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path), required=True, nargs=1)
+@click.argument('outdir', type=click.Path(file_okay=False, dir_okay=True, path_type=Path), required=True, nargs=1)
 @click.option('-l', '--label-num', 'lbnum', type=int, required=True, prompt='Label number to train the model on')
 @click.option('-m', '--model', 'model', type=click.Choice(['sage','gat'], case_sensitive=False), required=True, prompt='Choose GNN model', help='GNN model to use')
 def main(inputdir,outdir,lbnum:int, model:str):
@@ -140,7 +141,7 @@ def main(inputdir,outdir,lbnum:int, model:str):
 
 def runGruGat(ds:MapGraph,metadata:MetaData):
     model = GRUGAT(
-        dynamic_features_num=NUM_TEMPORAL_FEATURES,
+        dynamic_features_num=GG_NUM_TEMPORAL_FEATURES,
         has_dims=metadata.has_dims,
         gru_hidden_size=GG_GRU_HIDDEN_SIZE,
         gru_num_layers=GG_GRU_NUM_LAYERS,
@@ -175,7 +176,7 @@ def runGruGat(ds:MapGraph,metadata:MetaData):
 
 def runSage(ds:MapGraph,metadata:MetaData):
     model = GraphSAGEGraphLevel(
-        dynamic_features_num=NUM_TEMPORAL_FEATURES,
+        dynamic_features_num=SAGE_NUM_TEMPORAL_FEATURES,
         has_dims=metadata.has_dims,
         frames_num=metadata.frames_num,
         hidden_dims=SAGE_HIDDEN_DIMS,
--- src/gat.py
+++ src/gat.py
@@ -1,28 +1,89 @@
 import torch as _torch
 import torch.nn as _nn
-from torch_geometric.nn import GATConv as _GATConv, global_mean_pool as _gmpool
+from torch_geometric.nn import GATv2Conv as _GATv2Conv, global_mean_pool as _gmean_pool, global_max_pool as _gmax_pool
 import torch.nn.functional as _F
 from typing import Literal as _Lit
 
-class GATGraphLevel(_nn.Module):
-    def __init__(self, in_dim, hidden_dims=[128, 128], out_dim=10, num_st_types=256, emb_dim=12,*, heads=4, negative_slope=0.2, dropout=0.6):
+class GRUGAT(_nn.Module):
+    def __init__(self, dynamic_features_num, has_dims, gru_hidden_size=128,gru_num_layers=1, gat_edge_fnum:int|None=None,gat_inner_dims=[96, 96], gat_nheads=4, fc_dims=[50,50], out_dim=10, num_st_types=256, emb_dim=12,*, negative_slope=0.2, dropout=0.6):
         super().__init__()
+        
+
+        assert len(gat_inner_dims) >= 1, "gat_inner_dims must contain at least one element"
+        assert gat_nheads >= 1, "gat nheads must be at least 1"
+        assert gat_edge_fnum is None or gat_edge_fnum > 0, "gat_edge_fnum must be positive or None"
+
+        if gat_edge_fnum is None:
+            raise NotImplementedError("GAT without edge features is not implemented yet")
+
+        # 1. GRU layer to process dynamic features
+        # x shape: [batch_vnum, framenum, feature_dim]
+        self.gru = _nn.GRU(
+            input_size=dynamic_features_num,
+            hidden_size=gru_hidden_size,
+            num_layers=gru_num_layers,
+            batch_first=True
+        )
+
+        # 2. embedding for station types
         self.st_emb = _nn.Embedding(num_st_types, emb_dim)
-        dims = [in_dim + emb_dim] + hidden_dims
+
+        # 3. concat all input features
+        in_dim = gru_hidden_size + (2 if has_dims else 0) + emb_dim
+
+        # 4. GAT layers
+        gdims = [in_dim] + gat_inner_dims
         self.convs = _nn.ModuleList([
-            _GATConv(dims[i]*(1 if i==0 else heads), dims[i+1], heads=heads, concat=(i < len(hidden_dims)-1), negative_slope=negative_slope, dropout=dropout) for i in range(len(hidden_dims))
+            _GATv2Conv(
+                in_channels=gdims[i]*(1 if i==0 else gat_nheads),
+                out_channels=gdims[i+1],
+                heads=gat_nheads,
+                concat=True,
+                negative_slope=negative_slope,
+                edge_dim=gat_edge_fnum,
+                dropout=dropout
+            ) for i in range(len(gdims)-1)
         ])
-        self.lin = _nn.Linear(dims[-1], out_dim)
+
+        self.norms = _nn.ModuleList([
+            _nn.LayerNorm(gdims[i+1]*gat_nheads) for i in range(len(gdims)-1)
+        ])
+
+        # 5. final fc layers
+        ldims = [gat_inner_dims[-1]*gat_nheads*2] + fc_dims + [out_dim]
+        self.fcs = _nn.ModuleList([
+            *[ _nn.Sequential(
+                _nn.Linear(ldims[i], ldims[i+1]),
+                _nn.LeakyReLU(negative_slope=negative_slope),
+                _nn.Dropout(p=dropout)
+            ) for i in range(len(ldims)-2)],
+            _nn.Linear(ldims[-2], ldims[-1])
+        ])
+        self.dropout = dropout
 
     def forward(self, data):
-        x, edge_index, edge_attr, st_types_feats, batch = data.x, data.edge_index, data.edge_attr, data.st_types_feats, data.batch
-        st_embedded:_torch.Tensor = self.st_emb(st_types_feats)
-        st_embedded = st_embedded.squeeze(1)
-        x = _torch.cat([x, st_embedded], dim=1)
-        for i,conv in enumerate(self.convs):
-            x = conv(x, edge_index)
-            if i != len(self.convs) - 1:
-                x = _F.elu(x)
-        x = _gmpool(x, batch)
-        logits = self.lin(x)
-        return logits
+        x, edge_index, edge_attr, xdims, xsttype, batch = data.x, data.edge_index, data.edge_attr, data.xdims, data.xsttype, data.batch
+        st_embedded:_torch.Tensor = self.st_emb(xsttype)
+
+        # process dynamic features with GRU
+        # here data are in shape [batch_vnum, seq_len, feature_dim]
+        # get the last hidden state
+        _, hlast = self.gru(x)
+        hlast = hlast[-1, :, :] # take last layer's hidden state
+        # shape [batch_vnum, gru_hidden_size]
+
+        x = _torch.cat([hlast, xdims, st_embedded], dim=1)
+        for conv, norm in zip(self.convs, self.norms):
+            x = conv(x, edge_index, edge_attr)
+            x = norm(x)
+            x = _F.elu(x)
+            x = _F.dropout(x, p=self.dropout, training=self.training)
+
+        xmean = _gmean_pool(x, batch)
+        xmax = _gmax_pool(x, batch)
+        x = _torch.cat([xmean, xmax], dim=1)
+
+        # now fc layers
+        for fc in self.fcs:
+            x = fc(x)
+        return x
