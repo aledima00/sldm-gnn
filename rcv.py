350420b49113c791d89fec7181ae9089eb51d4fb
Alessandro Di Matteo <aledimatteo00@gmail.com>
2026-02-05 18:03:40 +0100

    updated multithreaded receiver

--- rcv.py
+++ rcv.py
@@ -3,29 +3,72 @@ import click
 import json
 import pandas as pd
 from pathlib import Path
+from collections import deque
+import threading
+
+MAX_JSON_CHUNK_SIZE = 32 * 1024  # 32KB ~ about 300 vehicles in each frame
+
+def pipeout_producer(fd: int, pack_queue: deque, pack_size:int,condition: threading.Condition, terminate_event: threading.Event):
+    buffer = ""
+    while not terminate_event.is_set():
+        chunk = os.read(fd, MAX_JSON_CHUNK_SIZE).decode()
+        print(f"CHUNKSIZE: {len(chunk)}")
+        if not chunk:
+            print("Writer has closed the FIFO. Exiting.")
+            terminate_event.set()
+            with condition:
+                condition.notify_all()  # Notify the consumer to exit if waiting
+            break
+        
+        buffer += chunk
+        while '\n' in buffer:
+            # cycle over the lines/frames
+            line, buffer = buffer.split('\n', 1)
+            if line.strip():
+                data = json.loads(line)
+                df = pd.DataFrame(data)
+                #print("received df:\n",df)
+                with condition:
+                    pack_queue.append(df)
+                    if len(pack_queue) >= pack_size:
+                        condition.notify()  # Notify the consumer that a pack is ready
+
+def infer_consumer(pack_queue: deque, pack_size:int, condition: threading.Condition,stride:int, terminate_event: threading.Event):
+    while not terminate_event.is_set():
+        with condition:
+            while (len(pack_queue) < pack_size) and not terminate_event.is_set():
+                condition.wait()  # Wait until enough frames are available
+            if not terminate_event.is_set():
+                packDf = pd.concat(list(pack_queue)[:pack_size], keys=range(1, pack_size+1), names=['FrameID'])
+        if not terminate_event.is_set():
+            print("packDf:\n", packDf)
+            with condition:
+                for _ in range(stride):
+                    if pack_queue:
+                        pack_queue.popleft()  # Remove frames based on stride
 
 @click.command()
-@click.option('-f', '--fifo-path', 'fifo_path', type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, type=Path), required=True, help='Path to the FIFO (named pipe) to read from.')
-def main(fifo_path: Path):
+@click.option('-f', '--fifo-path', 'fifo_path', type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, path_type=Path), required=True, help='Path to the FIFO (named pipe) to read from.')
+@click.option('-p', '--pack-size', 'pack_size', type=int, required=True, help='Number of frames to pack together before processing.')
+@click.option('-s', '--stride', 'stride', type=int, default=1, help='Number of frames to stride after each pack processing.')
+def main(fifo_path: Path, pack_size: int, stride: int):
     # apre la fifo in lettura (bloccante finché un writer non si connette)
     fd = os.open(fifo_path.resolve(),  os.O_RDONLY)
-    buffer = ""
+    pack_queue = deque()
+    lock = threading.Lock()
+    condition = threading.Condition(lock)
+    terminate_event = threading.Event()
+
     try:
-        while True:
-            chunk = os.read(fd, 4096).decode() #TODO: decide Bytes num (4096?)
-            if not chunk:
-                print("Writer has closed the FIFO. Exiting.")
-                break
-            
-            buffer += chunk
-            while '\n' in buffer:
-                line, buffer = buffer.split('\n', 1)
-                if line.strip():
-                    data = json.loads(line)
-                    df = pd.DataFrame(data)
-                    #print("received df:\n",df)
+        t1 = threading.Thread(target=pipeout_producer, args=(fd, pack_queue, pack_size, condition, terminate_event))
+        t2 = threading.Thread(target=infer_consumer, args=(pack_queue, pack_size, condition, stride, terminate_event))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
     finally:
         os.close(fd)
+        print("Bye!")
 
 if __name__ == "__main__":
     main()
