1aba2371d0a4867394b98d0808823f74b101f7cf
Alessandro Di Matteo <aledimatteo00@gmail.com>
2026-05-05 14:06:29 +0200

    corrected labels

--- src/labels.py
+++ src/labels.py
@@ -2,10 +2,6 @@ from enum import IntEnum as _IE
 
 class LabelsEnum(_IE):
     LANE_CHANGE = 0
-    EMERGENCY_BRAKING = 1
-    OVERTAKE = 2
-    TURN_INTENT = 3
-    COLLISION = 4
-    LANE_MERGE = 5
-    OBSTACLE_IN_ROAD = 6
-    SLOWDOWN = 7
+    OVERTAKE = 1
+    TURN = 2
+    COLLISION = 3
